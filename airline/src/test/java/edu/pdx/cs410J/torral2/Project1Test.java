2ae897cfb630137ed7ca3abc729496104e8c76b7
christian <cjtorralba@gmail.com>
2023-01-25 11:48:57 -0800

    Added some more unit tests for Flight class

--- airline/src/test/java/edu/pdx/cs410J/torral2/Project1Test.java
+++ airline/src/test/java/edu/pdx/cs410J/torral2/Project1Test.java
@@ -31,10 +31,51 @@ class Project1Test {
 
 
   @Test
-  void getFlightNumberTest() {
-
- // Flight testFlight = new Flight();
-
-
+  void testGetFlightNumber() {
+    int testFlightNumber = 123;
+    Flight testFlight = new Flight(testFlightNumber, "PDX", "10/23/2022", "12:45", "LAX", "10/24/2022", "1:15");
+    assertThat(testFlightNumber, equalTo(testFlight.getNumber()));
   }
+
+
+  @Test
+  void testGetDepartureString() {
+    String departeTime = "10:53";
+    String departureDate = "10/24/2023";
+    String departureDateAndTime = departureDate + " " + departeTime;
+
+    Flight testFlight = new Flight(722, "PDX",departureDate, departeTime, "LAX", "10/24/2022", "1:15");
+
+    assertThat(departureDateAndTime, containsString(testFlight.getDepartureString()));
+  }
+
+  @Test
+  void testGetArrivalString() {
+    String arriveTime = "10:53";
+    String arriveDate = "10/24/2023";
+    String arriveDateAndTime = arriveDate + " " + arriveTime;
+
+    Flight testFlight = new Flight(722, "PDX", "10/23/2021", "3:53", "LAX", arriveDate, arriveTime);
+
+    assertThat(arriveDateAndTime, containsString(testFlight.getArrivalString()));
+  }
+
+
+  @Test
+  void testGetSource() {
+    String src = "SOURCE";
+    Flight testFlight = new Flight(123, src, "10/12/1234", "10:23", "PDX", "11/23/2009", "11:20");
+    assertThat(src, containsString(testFlight.getSource()));
+  }
+
+
+  @Test
+  void testGetDestination() {
+    String dest = "DESTINATION";
+    Flight testFlight = new Flight(123, "PDX", "10/12/1234", "10:23", dest, "1/23/2009", "11:20");
+    assertThat(dest, containsString(testFlight.getDestination()));
+  }
+
+
 }
+
