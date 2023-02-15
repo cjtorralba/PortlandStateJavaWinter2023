7fc637ba200ece88ebdfcb75769b96823bd81a7f
christian <cjtorralba@gmail.com>
2023-02-15 10:04:11 -0800

    Added necessary files for project 4

---
+++ airline/src/main/java/edu/pdx/cs410J/torral2/XmlDumper.java
@@ -0,0 +1,15 @@
+package edu.pdx.cs410J.torral2;
+
+import edu.pdx.cs410J.AbstractAirline;
+import edu.pdx.cs410J.AirlineDumper;
+
+import java.io.IOException;
+
+public class XmlDumper implements AirlineDumper<Airline> {
+
+
+    @Override
+    public void dump(Airline airline) throws IOException {
+
+    }
+}
---
+++ airline/src/main/java/edu/pdx/cs410J/torral2/XmlParser.java
@@ -0,0 +1,12 @@
+package edu.pdx.cs410J.torral2;
+
+import edu.pdx.cs410J.AbstractAirline;
+import edu.pdx.cs410J.AirlineParser;
+import edu.pdx.cs410J.ParserException;
+
+public class XmlParser implements AirlineParser {
+    @Override
+    public AbstractAirline parse() throws ParserException {
+        return null;
+    }
+}
