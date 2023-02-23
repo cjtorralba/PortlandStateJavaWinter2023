e6ed97a416cf1c5be4ef21a4de5365eab031be72
cjtorralba <cjtorralba@gmail.com>
2023-02-22 23:54:13 -0800

    Fixed test

--- airline/src/test/java/edu/pdx/cs410J/torral2/XmlParserTest.java
+++ airline/src/test/java/edu/pdx/cs410J/torral2/XmlParserTest.java
@@ -4,6 +4,7 @@ import edu.pdx.cs410J.ParserException;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.InputStream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -24,7 +25,9 @@ public class XmlParserTest {
     */
    @Test
    void testXmlParseValidAirline() throws ParserException {
-      XmlParser xmlParser = new XmlParser(new File("C:\\Users\\cjtorralba\\IdeaProjects\\PortlandStateJavaWinter2023\\airline\\src\\test\\resources\\edu\\pdx\\cs410J\\torral2\\valid-airline.xml"));
+
+      File is = new File(getClass().getResource("valid-airline.xml").getFile());
+      XmlParser xmlParser = new XmlParser(is);
       Airline airline = xmlParser.parse();
 
       assertThat(airline.getName(), equalTo("Valid Airlines"));
