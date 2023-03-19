34bb69ef1fc56845461ac8186204218a0db71d35
christian <cjtorralba@gmail.com>
2023-03-19 04:04:16 -0700

    Added cool stream way to search all flights coming to a specific destination leaving another

--- airline-android/app/src/main/java/edu/pdx/cs410J/torral2/SearchFlightActivity.java
+++ airline-android/app/src/main/java/edu/pdx/cs410J/torral2/SearchFlightActivity.java
@@ -1,10 +1,18 @@
 package edu.pdx.cs410J.torral2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+import edu.pdx.cs410J.ParserException;
 
 public class SearchFlightActivity extends AppCompatActivity {
 
@@ -19,8 +27,46 @@ public class SearchFlightActivity extends AppCompatActivity {
 
 
 
-    public void searchFlights() {
+    public void searchFlights(View view) {
 
+        Airline airline;
+        ArrayList<Flight> listOfFlights = new ArrayList<>();
+
+        LinearLayout linearLayout = findViewById(R.id.searchFlightLayout);
+
+        EditText airlineName = linearLayout.findViewById(R.id.editSearchFlightAirlineName);
+        String airlineNameText = airlineName.getText().toString();
+
+        EditText source = linearLayout.findViewById(R.id.editSearchFlightAirportSource);
+        String sourceText = source.getText().toString();
+
+        EditText destination = linearLayout.findViewById(R.id.editSearchFlightAirportDestination);
+        String destinationText = destination.getText().toString();
+
+        if(airlineNameText.trim().isEmpty()) { // User did not supply airline name, searching all airlines
+            File fileDirectory = getFilesDir();
+            for(File file : fileDirectory.listFiles()) {
+                if(file != null) {
+                    try {
+                         listOfFlights.addAll(new XmlParser(file).parse()
+                                .getFlights()
+                                .stream()
+                                .filter( f -> f.getSource().equals(sourceText) && f.getDestination().equals(destinationText))
+                                .collect(Collectors.toList()));
+                    } catch (ParserException pe) {
+                        Toast.makeText(this, "Could not parse file: " + file.getAbsolutePath(), Toast.LENGTH_LONG).show();
+                    }
+                }
+
+            }
+            for (Flight f : listOfFlights) {
+                Toast.makeText(this, f.getFlightAsTextFileString(), Toast.LENGTH_SHORT).show();
+            }
+
+        } else { // User DID supply airline name, so we will only be searching flights connected to specified airline
+
+
+        }
 
 
     }
